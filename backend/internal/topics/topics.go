b8e9f432a5b6a98ac370bd3e170e543c84f0761a
Alvin Antonius <alvin.antonius.siskom@gmail.com>
2017-07-02 15:35:12 +0700

    inital commit, create app structure

---
+++ backend/internal/topics/topics.go
@@ -0,0 +1,63 @@
+package topics
+
+import ()
+
+type (
+	// PkgTopics is the interfaces abstraction for any package that import it
+	// to interact with this package
+	PkgTopics interface {
+	}
+
+	// pkgTopics is the
+	pkgTopics struct {
+	}
+
+	// Topic it the main data object
+	// represent post in our app
+	Topic struct {
+		ID      int64  `json:"topic_id"`
+		Title   string `json:"topic_title"`
+		Upvotes int64  `json:"topic_upvotes"`
+	}
+)
+
+// tSequence is for uniqueID, will auto increment as new topic is created
+// start from 0
+var tSequence int64
+
+// data is our in memory data storage for storing all topics data in the app
+// the index is from tSequence value
+var data map[int64]Topic
+
+// descSorted is our slice of pointer that contains list of sorted topic based on the upvotes
+// sorted in descending
+var descSorted []*Topic
+
+// init for intialize our empty map for data storage
+func init() {
+	tSequence = 0
+	data = make(map[int64]Topic)
+	descSorted = []*Topic{}
+}
+
+// New is for creating our new package obj
+func New() PkgTopics {
+	return &pkgTopics{}
+}
+
+// CreateTopic is for creating new topics
+// will return created Topic object
+func (pkg *pkgTopics) CreateTopic(title string) Topic {
+
+}
+
+// List will get the list of the topic
+// sorted by upvotes
+func (pkg *pkgTopics) List(take, skip int64) []Topic {
+
+}
+
+// GetTopic is for get single topic data based of the id
+func (pkg *pkgTopics) GetTopic(id int64) Topic {
+
+}
